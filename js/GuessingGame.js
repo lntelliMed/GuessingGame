7afdb26778828a65406ab1ccb770bd426467ca38
“Sam <sam.alsmadi@gmail.com>
2017-10-18 20:59:40 -0400

    Add event listeners on reset/hint buttons

--- js/GuessingGame.js
+++ js/GuessingGame.js
@@ -28,7 +28,7 @@ Game.prototype.isLower = function(){
 }
 
 Game.prototype.provideHint = function(){
-    return shuffle([this.winningNumber,generateWinningNumber(),generateWinningNumber()]);
+    return shuffle([this.winningNumber, generateWinningNumber(), generateWinningNumber()]);
 }
 
 Game.prototype.playersGuessSubmission = function(num){
@@ -72,4 +72,86 @@ function newGame(){
     return new Game();
 }    
 
- 
+//  $(document).ready(function(){
+//     var game = newGame();    
+//     $("#submit").on("click", function(event){
+//         console.log("Submit button was clicked!");        
+//         processUserValue();
+//     });
+
+//     $("#players-input").on("keyup", function(event){
+//         //console.log(event.which);
+//         if(event.which === 13){
+//             console.log("Enter was pressed!");                    
+//             processUserValue();
+//         }
+//     });
+
+//     function processUserValue(){
+//         var usersValue = +$("#players-input").val();
+//         $("#players-input").val("");
+//         console.log(usersValue);
+//         game.playersGuessSubmission(usersValue);
+//     }
+//  });
+
+function makeAGuess(game) {
+    var guess = $('#players-input').val();    
+    $('#players-input').val("");
+    var output = game.playersGuessSubmission(parseInt(guess,10));
+    console.log(output);
+    $("#title").text(output);
+
+    if(output === "You Win!" || output === "You Lose."){
+        $("#submit").prop( "disabled", true );
+        $("#hint").prop( "disabled", true );        
+        $("#players-input").prop( "disabled", true );                
+        $("#subtitle").text("Press the Reset button!");                
+    } else if(output === "You have already guessed that number."){
+        $("#subtitle").text("Guess again!");                
+    } else if(game.isLower()){
+        $("#subtitle").text("Guess a higher number!");
+    } else {
+        $("#subtitle").text("Guess a lower number!");                
+    }
+    
+    $(".guess").each(function(i){
+        $(this).text(game.pastGuesses[i]);
+    });
+
+}
+
+$(document).ready(function() {
+    var game = new Game();
+    //var game = newGame();    
+    var initialTitle = $("#title").text();
+    var initialSubTitle = $("#subtitle").text();
+    var initialGuessList = $(".guess:first").text();
+    
+
+    $('#submit').click(function(e) {
+       makeAGuess(game);
+    });
+
+    $('#players-input').keypress(function(event) {
+        if (event.which === 13) {            
+           makeAGuess(game);
+        }
+    });
+
+    $('#reset').click(function(e) {
+        $("#submit").prop( "disabled", false );
+        $("#hint").prop( "disabled", false );        
+        $("#players-input").prop( "disabled", false );                
+        $("#title").text(initialTitle); 
+        $("#subtitle").text(initialSubTitle);  
+        $(".guess").each(function(i){
+            $(this).text(initialGuessList);
+        });            
+        game = new Game();          
+    });
+
+    $('#hint').click(function(e) {
+        $("#title").text("The correct answer is one of the following: " + game.provideHint());
+    });
+});
